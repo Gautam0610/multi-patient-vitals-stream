9ef6923b4fddcc52d989232be86ea87073e38c1a
Gautam <167000391+Gautam0610@users.noreply.github.com>
2025-12-23 16:10:22 +0530

    Create config.py

---
+++ utils/config.py
@@ -0,0 +1,5 @@
+
+NUM_PATIENTS = 1000
+KAFKA_BROKER_URLS = ['localhost:9092']  # Replace with your Kafka broker URLs
+KAFKA_TOPIC = 'patient_vitals'
+EVENT_SIMULATION_INTERVAL = 60  # Simulate events every 60 seconds
